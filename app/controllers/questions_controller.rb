fafd9f5edbe4556f53ca4a1b1aa854511f6dd5ab
Juliette Chevalier <juliettech.1393@gmail.com>
2019-02-18 11:41:14 +1100

    updated/perfected the logic for my stupid coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,25 +1,16 @@
 class QuestionsController < ApplicationController
   def ask
-    # params[:question]
-    # @question = params[:question]
   end
 
   def answer
-    params[:user_question]
-    @question = params[:user_question]
-
-
-    if @question == "I am going to work"
+    @question = params[:question]
+    if @question == "I am going to work" || "I AM GOING TO WORK"
       @answer = "Great!"
     elsif @question.include?("?")
       @answer = "Silly question, get dressed and go to work!"
     else
       @answer = "I dont care, get dressed and go to work!"
     end
-
-    if @answer
-  end
-
-  def home
+    @answer
   end
 end
